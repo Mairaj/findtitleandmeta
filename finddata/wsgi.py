eebbfa0b6fd6aa0d4581a987ecce5ffd699a5d6f
mairaj <mairajkhan00@gmail.com>
2013-09-30 21:07:12 +0530

    test

--- finddata/wsgi.py
+++ finddata/wsgi.py
@@ -25,5 +25,4 @@ application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
- 	
+# application = HelloWorldApplication(application
